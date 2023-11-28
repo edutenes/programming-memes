b8f73b1d04e56b9a29488831ff013a03847abff9
Edu Tenes <eduardotenes123@gmail.com>
2023-11-28 09:15:00 +0100

    Primera Versión Azure

--- bot.js
+++ bot.js
@@ -1,11 +1,3 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-const config = new Configuration({
-	apiKey: "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",
-});
-
-
-
 const { TwitterApi } = require('twitter-api-v2');
 require('dotenv').config();
 const axios = require('axios');
@@ -21,40 +13,110 @@ const twitterClient = new TwitterApi({
 });
 const rwClient = twitterClient.readWrite;
 
-// Función para obtener la imagen con el número más bajo en la carpeta memes/1
-function getLowestNumberedMemePath() {
-  const memesDir = path.join(__dirname, 'memes', '1');
-  const files = fs.readdirSync(memesDir);
-  if (files.length === 0) {
-    console.log('No hay más memes para publicar.');
-    return null;
-  }
-  const sortedFiles = files.sort((a, b) => parseInt(a) - parseInt(b));
-  return path.join(memesDir, sortedFiles[0]);
+// Cargar archivos JSON
+const frasesDebate = JSON.parse(fs.readFileSync(path.join(__dirname, 'frases-debate.json'), 'utf8'));
+const frasesGraciosas = JSON.parse(fs.readFileSync(path.join(__dirname, 'frases-graciosas.json'), 'utf8'));
+const frasesMemes = JSON.parse(fs.readFileSync(path.join(__dirname, 'frases-memes.json'), 'utf8'));
+
+
+// Cargar o inicializar el contador
+let contador;
+try {
+  contador = JSON.parse(fs.readFileSync(path.join(__dirname, 'contador.json'), 'utf8'));
+} catch (error) {
+  contador = { count: 0 }; // Inicializar si el archivo no existe
 }
 
-// Función para mover la imagen a la carpeta memes/2 después de publicar
-function moveMemeToPublished(memePath) {
-  const publishedDir = path.join(__dirname, 'memes', '2');
-  const publishedPath = path.join(publishedDir, path.basename(memePath));
-  fs.renameSync(memePath, publishedPath);
-  console.log(`Meme movido a: ${publishedPath}`);
-}
-
-// Función modificada para publicar el meme en Twitter
 async function tweetMeme() {
-  const memePath = getLowestNumberedMemePath();
-  if (memePath) {
+  const memesDir = path.join(__dirname, 'memes', '1');
+  const files = fs.readdirSync(memesDir).sort((a, b) => {
+    // Extraer el número del nombre del archivo y convertirlo a entero
+    const numA = parseInt(a.split('.')[0]);
+    const numB = parseInt(b.split('.')[0]);
+    return numA - numB;
+  });
+
+  if (contador.ultimoMeme >= files.length) {
+    console.log('No hay más memes para publicar.');
+    return;
+  }
+
+  const memeFileName = files[contador.ultimoMeme];
+  const memePath = path.join(memesDir, memeFileName);
+  const titulo = frasesMemes[memeFileName] || 'Título por defecto'; // Usar título por defecto si no se encuentra
+
+  try {
+    // Publicar el tweet con el meme
+    const mediaId = await rwClient.v1.uploadMedia(memePath);
+    await rwClient.v2.tweet({ text: titulo, media: { media_ids: [mediaId] } });
+    console.log(`Meme publicado: ${titulo}`);
+
+    contador.ultimoMeme++;
+    // Guardar el estado actualizado del contador
+    // (Asegúrate de implementar esta parte según tus necesidades)
+  } catch (error) {
+    console.error('Error al publicar en Twitter:', error);
+  }
+}
+
+
+async function tweetFrasesDebate() {
+  if (contador.ultimaFraseDebate < frasesDebate.length) {
+    const fraseDebate = frasesDebate[contador.ultimaFraseDebate];
+    
     try {
-      const mediaId = await rwClient.v1.uploadMedia(memePath);
-      await rwClient.v2.tweet({ text: 'JavaScript, My Favorite Language...', media: { media_ids: [mediaId] } });
-      console.log('Meme publicado en Twitter');
-      moveMemeToPublished(memePath);
+      // Publicar el tweet
+      await rwClient.v2.tweet({ text: fraseDebate });
+      console.log(`Frase de Debate publicada: ${fraseDebate}`);
+      contador.ultimaFraseDebate++;
+      // Guardar el estado actualizado del contador
+      // (Asegúrate de implementar esta parte según tus necesidades)
     } catch (error) {
       console.error('Error al publicar en Twitter:', error);
     }
+  } else {
+    console.log('No hay más frases de debate para publicar.');
   }
 }
 
-// Ejecutamos la función tweetMeme
-tweetMeme();
+
+async function tweetFrasesGraciosas() {
+  if (contador.ultimaFraseGraciosa < frasesGraciosas.length) {
+    const fraseGraciosa = frasesGraciosas[contador.ultimaFraseGraciosa];
+    
+    try {
+      // Publicar el tweet
+      await rwClient.v2.tweet({ text: fraseGraciosa });
+      console.log(`Frase Graciosa publicada: ${fraseGraciosa}`);
+      contador.ultimaFraseGraciosa++;
+      // Guardar el estado actualizado del contador
+      // (Asegúrate de implementar esta parte según tus necesidades)
+    } catch (error) {
+      console.error('Error al publicar en Twitter:', error);
+    }
+  } else {
+    console.log('No hay más frases graciosas para publicar.');
+  }
+}
+
+
+// Función principal para alternar entre tipos de contenido
+async function tweetContent() {
+  switch (contador.count % 3) {
+    case 0:
+      await tweetMeme();
+      break;
+    case 1:
+      await tweetFrasesDebate();
+      break;
+    case 2:
+      await tweetFrasesGraciosas();
+      break;
+  }
+
+  // Incrementar y guardar el contador
+  contador.count = (contador.count + 1) % 3; // Esto reiniciará el contador después de 3
+  fs.writeFileSync(path.join(__dirname, 'contador.json'), JSON.stringify(contador, null, 2));
+}
+
+tweetContent();
--- index.js
+++ index.js
@@ -1,10 +1,65 @@
 const fs = require('fs');
 const path = require('path');
 
-let memesDictionary = {};
+// Ruta del archivo JSON para el contador
+const contadorFilePath = path.join(__dirname, 'contador.json');
 
-for (let i = 331; i <= 1949; i++) {
-  memesDictionary[`${i}.png`] = ``;
+// Función para obtener el valor actual del contador desde el archivo JSON
+function getContador() {
+  try {
+    const contadorData = fs.readFileSync(contadorFilePath, 'utf8');
+    const contador = JSON.parse(contadorData);
+    return contador.count;
+  } catch (error) {
+    // Si el archivo no existe o hay un error al leerlo, iniciar el contador en 0
+    return 0;
+  }
 }
 
-fs.writeFileSync(path.join(__dirname, 'frases-memes.json'), JSON.stringify(memesDictionary, null, 2));
+// Función para incrementar el contador y guardarlo en el archivo JSON
+function incrementarContador() {
+  const contador = getContador() + 1;
+  const contadorData = { count: contador };
+
+  fs.writeFileSync(contadorFilePath, JSON.stringify(contadorData, null, 2), 'utf8');
+}
+
+// Variables para mantener un registro de frases y rutas de imágenes utilizadas
+let currentIndex = getContador();
+
+// Cargar las listas de frases desde los archivos JSON
+const frasesMemes = require('./frases-memes.json');
+const frasesGraciosas = require('./frases-graciosas.json');
+const frasesDebate = require('./frases-debate.json');
+
+// Función para obtener la siguiente frase y su imagen asociada en el orden específico
+function getNextMemeAndPhrase() {
+  if (currentIndex % 3 === 0) {
+    return { frase: frasesMemes['331.png'], imagePath: 'memes/1/331.png' };
+  } else if (currentIndex % 3 === 1) {
+    return { frase: frasesGraciosas[0], imagePath: getImagePath('332.png') };
+  } else {
+    return { frase: frasesDebate[0], imagePath: getImagePath('334.png') };
+  }
+}
+
+// Función para obtener la ruta de la imagen
+function getImagePath(imageName) {
+  return path.join(__dirname, 'memes', '1', imageName);
+}
+
+// Función para probar la selección de frases e imágenes en el orden deseado
+function testMemeSelection() {
+  const { frase, imagePath } = getNextMemeAndPhrase();
+
+  if (frase && imagePath) {
+    console.log('Frase seleccionada:', frase);
+    console.log('Ruta de la imagen seleccionada:', imagePath);
+  }
+
+  // Incrementar el contador y guardarlo en el archivo JSON
+  incrementarContador();
+}
+
+// Ejecutamos la función testMemeSelection
+testMemeSelection();
